31334894937955f74dc74b91e6f73b4fcea8bb32
Eugene <traffox@protonmail.com>
2021-01-27 18:19:12 +0300

    api

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 db = SQLAlchemy(app)
-
+api = Api(app)
 from app.catalog.views import catalog
 
 app.register_blueprint(catalog)
