a7429d4346c4112c8c1bc02fba264e9577528168
Eugene <traffox@protonmail.com>
2021-01-29 09:55:13 +0300

    updated requirements, dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,14 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+import os
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost/postgres"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = True
-# app.config["SECRET_KEY"] = True
-app.config.from_envvar("ENV")
+key = os.getenv("JWT_SECRET_KEY")
+app.config["SECRET_KEY"] = f"{key}"
 
 db = SQLAlchemy(app)
 api = Api(app)
